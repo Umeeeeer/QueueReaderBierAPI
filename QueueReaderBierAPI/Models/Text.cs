9dcb57ddc2588e634e9a80b0973779c732573110
umeer arshad <umeer.arshad@hotmail.com>
2018-09-26 14:08:50 +0200

    gsgsg
    sgsgsgs

--- QueueReaderBierAPI/Models/Text.cs
+++ QueueReaderBierAPI/Models/Text.cs
@@ -11,5 +11,12 @@ namespace QueueReaderBierAPI.Models
         public string text { get; set; }
         public float x { get; set; }
         public float y { get; set; }
+
+        public Text(string text, float x, float y)
+        {
+            this.text = text;
+            this.x = x;
+            this.y = y;
+        }
     }
 }
